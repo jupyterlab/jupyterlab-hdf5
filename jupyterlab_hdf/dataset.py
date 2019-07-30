66a3714704c1deac4ccd2182675937eb830a1547
telamonian <mklein@jhu.edu>
2019-07-30 05:02:57 -0400

    dataset api tested and working

--- jupyterlab_hdf/dataset.py
+++ jupyterlab_hdf/dataset.py
@@ -1,7 +1,6 @@
 import h5py
 import json
 import os
-import re
 from tornado import gen
 from tornado.httpclient import HTTPError
 
@@ -18,12 +17,52 @@ def datasetDict(data):
 def getDatasetHdf(dset, rslice, cslice):
     return [datasetDict(dset[rslice, cslice].tolist())]
 
+class HdfDatasetManager:
+    """Implements HDF5 dataset data/error handling
+    """
+    def __init__(self, notebook_dir):
+        self.notebook_dir = notebook_dir
+
+    def get(self, path, uri, row, col):
+        def _handleErr(code, msg, path, uri, row, col):
+            raise HTTPError(code, '\n'.join((
+                msg,
+                f'fpath: {path}, uri: {uri}, row: {row}, col: {col}'
+            )))
+
+        if not path:
+            msg = f'The request was malformed; fpath should not be empty.'
+            _handleErr(400, msg, path, uri, row, col)
+
+        fpath = url_path_join(self.notebook_dir, path)
+
+        if not os.path.exists(fpath):
+            msg = f'Request cannot be completed; no file at fpath.'
+            _handleErr(403, msg, path, uri, row, col)
+        else:
+            try:
+                # test opening the file with h5py
+                with h5py.File(fpath, 'r') as f: pass
+            except Exception as e:
+                msg = (f'The file at fpath could not be opened by h5py.\n'
+                       f'Error: {e}')
+                _handleErr(401, msg, path, uri, row, col)
+            try:
+                with h5py.File(fpath, 'r') as f:
+                    out = getDatasetHdf(f[uri], slice(*row), slice(*col))
+            except Exception as e:
+                msg = (f'Opened the file at fpath but could not retrieve valid dataset from {uri}.\n'
+                       f'Error: {e}')
+                _handleErr(500, msg, path, uri, row, col)
+
+            return out
+
 class HdfDatasetHandler(APIHandler):
     """A handler for HDF5 datasets
     """
-
     def initialize(self, notebook_dir):
         self.notebook_dir = notebook_dir
+        self.dataset_manager = HdfDatasetManager(notebook_dir=notebook_dir)
 
     @gen.coroutine
     def get(self, path):
@@ -31,41 +70,16 @@ class HdfDatasetHandler(APIHandler):
         serialized JSON.
         """
         uri = '/' + self.get_query_argument('uri').lstrip('/')
-        row = self.get_query_argument('row')
-        col = self.get_query_argument('col')
+        row = [int(x) for x in self.get_query_arguments('row')]
+        col = [int(x) for x in self.get_query_arguments('col')]
 
         try:
-            if not path:
-                msg = f'The request was malformed; fpath should not be empty.'
-                raise HTTPError(400, msg)
-
-            fpath = url_path_join(self.notebook_dir, path)
-
-            if not os.path.exists(fpath):
-                msg = f'Request cannot be completed; no file at fpath.'
-                raise HTTPError(403, msg)
-            else:
-                try:
-                    # test opening the file with h5py
-                    with h5py.File(fpath, 'r') as f: pass
-                except Exception as e:
-                    msg = (f'The file at fpath could not be opened by h5py.\n'
-                           f'Error: {e}')
-                    raise HTTPError(401, msg)
-                try:
-                    with h5py.File(fpath, 'r') as f:
-                        out = getDatasetHdf(f[uri], slice(*row), slice(*col))
-                except Exception as e:
-                    msg = (f'Opened the file at fpath but could not retrieve valid metadata from {uri}.\n'
-                           f'Error: {e}')
-                    raise HTTPError(500, msg)
-
-            self.finish(json.dumps(out))
+            self.finish(json.dumps(self.dataset_manager.get(path, uri, row, col)))
 
         except HTTPError as err:
             self.set_status(err.code)
-            message = err.response.body if err.response else str(err.code)
+            response = err.response.body if err.response else str(err.code)
             self.finish('\n'.join((
-                message,
-                f'fpath: {path}, uri: {uri}, row: {row}, col: {col}'
+                response,
+                err.message
             )))
