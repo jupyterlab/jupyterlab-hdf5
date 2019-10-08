899b60adc56baabeace09500551949f3362b300c
telamonian <mklein@jhu.edu>
2019-10-08 19:30:06 -0400

    added default value and tooltip to slice input
    this will be npmjs release 0.2.0

--- src/toolbar.tsx
+++ src/toolbar.tsx
@@ -69,7 +69,7 @@ export class SliceInputBox extends React.Component<
   constructor(props: SliceInput.IProps) {
     super(props);
     this.state = {
-      value: "",
+      value: ":, :",
       hasFocus: false
     };
   }
@@ -115,7 +115,7 @@ export class SliceInputBox extends React.Component<
 
   render() {
     return (
-      <label>
+      <label title={"Use Numpy syntax. 2D slices only"}>
         {"Slice: "}
         <input
           type="text"
