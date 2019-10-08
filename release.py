4560d1486979974fcfc061ed5ee271088f6f3d68
telamonian <mklein@jhu.edu>
2019-10-08 19:43:51 -0400

    release.py now working well, has `--dry` option for dryrun

--- release.py
+++ release.py
@@ -1,40 +1,28 @@
 #!/usr/bin/env python
 
 import argparse as argp
+import io
 import json
+import os
 import subprocess
 
-from setupbase import get_version
+__all__ = ['npmjs', 'pypi', 'tag']
 
-def tag(version, kind):
-    """git tagging
+versionFilePy = 'jupyterlab_hdf/_version.py'
+
+
+def _get_version_py(file, name='__version__'):
+    """Get the version of the package from the given file by
+    executing it and extracting the given `name`.
     """
-    tag = f"{kind}_v{version}"
+    path = os.path.realpath(file)
+    version_ns = {}
+    with io.open(path, encoding="utf8") as f:
+        exec(f.read(), {}, version_ns)
+    return version_ns[name]
 
-    subprocess.run(['git', 'tag', tag])
-    subprocess.run(['git', 'push', 'origin', tag])
 
-def pypi():
-    """release on pypi
-    """
-    # get single source of truth
-    version = get_version('jupyterlab_hdf/_version.py')
-
-    tag(version, 'pypi')
-
-    # build the tar release and binary wheels
-    # subprocess.run(['python', 'setup.py', 'sdist', 'bdist_wheel'])
-
-    # build just the tar release
-    subprocess.run(['python', 'setup.py', 'sdist'])
-
-    # release to the test server
-    # subprocess.run(['twine', 'upload', '--repository-url', 'https://test.pypi.org/legacy/', 'dist/*'])
-
-    # release to the production server
-    subprocess.run(['twine', 'upload', 'dist/*'])
-
-def npmjs():
+def npmjs(dry=False):
     """release on npmjs
     """
     # get single source of truth
@@ -43,24 +31,66 @@ def npmjs():
 
     version = info['version']
 
-    tag(version, 'npmjs')
+    tag(version, 'npmjs', dry=dry)
 
-    # dry run build and release
-    # subprocess.run(['npm', 'publish', '--access', 'public', '--dry-run'])
+    if dry:
+        # dry run build and release
+        subprocess.run(['npm', 'publish', '--access', 'public', '--dry-run'])
+    else:
+        # build and release
+        subprocess.run(['npm', 'publish', '--access', 'public'])
 
-    # build and release
-    subprocess.run(['npm', 'publish', '--access', 'public'])
+
+def pypi(dry=False):
+    """release on pypi
+    """
+    # get single source of truth
+    version = _get_version_py(versionFilePy)
+
+    tag(version, 'pypi', dry=dry)
+
+    # build the tar release and binary wheels
+    # subprocess.run(['python', 'setup.py', 'sdist', 'bdist_wheel'])
+
+    # build just the tar release
+    subprocess.run(['python', 'setup.py', 'sdist'])
+
+    if not dry:
+        # release to the test server
+        # subprocess.run(['twine', 'upload', '--repository-url', 'https://test.pypi.org/legacy/', 'dist/*'])
+
+        # release to the production server
+        subprocess.run(['twine', 'upload', 'dist/*'])
+
+
+def tag(version, kind, dry=False):
+    """git tagging
+    """
+    tag = f"{kind}_v{version}"
+
+    if dry:
+        print(f'would release with git tag: {tag}')
+    else:
+        subprocess.run(['git', 'tag', tag])
+        subprocess.run(['git', 'push', 'origin', tag])
+
 
 def main():
     parser = argp.ArgumentParser()
 
+    parser.add_argument('-d', '--dry', action='store_true')
     parser.add_argument('--pypi', action='store_true')
     parser.add_argument('--npmjs', action='store_true')
 
     parsed = vars(parser.parse_args())
-
-    if 'pypi' in parsed and parsed['pypi']:
-        pypi()
+    dry = 'dry' in parsed and parsed['dry']
 
     if 'npmjs' in parsed and parsed['npmjs']:
-        npmjs()
+        npmjs(dry=dry)
+
+    if 'pypi' in parsed and parsed['pypi']:
+        pypi(dry=dry)
+
+
+if __name__=='__main__':
+    main()
